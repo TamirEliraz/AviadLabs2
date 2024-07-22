420f8fff921367440cfa9f00dc4d39a3fe1ab341
TamirEliraz <tamire7972@gmail.com>
2024-07-22 20:51:17 +0300

    add blank classes from git - for labs 8, 9, 10

---
+++ src/main/java/lab8/AVLTree.java
@@ -0,0 +1,27 @@
+package lab8;
+
+public class AVLTree<T extends Comparable<T>> {
+    public AVLTree(T value) { }
+    
+    /**
+     * Add the value to the tree
+     *
+     * @return the updated root of the tree.
+     */
+    public AVLTree<T> add(T value) { return null; }
+    
+    /**
+     * @return the value in this node
+     */
+    public T getValue() { return null; }
+    
+    /**
+     * @return the left subTree of this node
+     */
+    public AVLTree<T> getLeft() { return null; }
+    
+    /**
+     * @return the right subTree of this node
+     */
+    public AVLTree<T> getRight() { return null; }
+}
